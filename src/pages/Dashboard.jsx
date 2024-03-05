c4699b89aa2cc20cfef9bdc904076f583f472662
Hussain Ali <76788374+hussain301@users.noreply.github.com>
2024-03-04 20:48:00 -0800

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,8 @@ import { useUserRecentStays } from "../features/dashboard/userRecentStays";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import Spinner from "../ui/Spinner";
+import Uploader from '../data/Uploader'
+
 
 function Dashboard() {
   const { bookings, isLoading, } = useUserRecentBookings()
@@ -20,6 +22,7 @@ function Dashboard() {
       <DashboardFilter/>
     </Row>
     <DashboardLayout />
+      <Uploader/>
     </>
   );
 }
